efd8e3054ce1572baf1d3c40d4560597fd9f00db
Tan Nhat <nguyentannhat246@gmail.com>
2018-03-13 20:44:05 +0700

    first file

---
+++ demo1.js
@@ -0,0 +1,20 @@
+var a = 100;
+var b = "Nhat";
+var c = true;
+var d = { ho: "Nguyen", ten: "Nhat" };
+var e = 101;
+{
+    var f = 99;
+    //let f:number = 99;(k tìm thấy)
+}
+//let bảo mật k toàn cục như var
+if (c) {
+    console.log("True");
+}
+else {
+    console.log("False");
+}
+console.log("-----------FOR---------");
+for (var i = 0; i < 5; i++) {
+    console.log("FOR" + i);
+}
